01deb6bdb63c138e194a1bd844f872dd26d077db
Marcus Rugger <marcusrugger@yahoo.com>
2014-12-14 18:11:16 -0500

    Create Commute model

---
+++ spec/factories/commutes.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :commute do
+    start_time 1
+end_time 1
+objective "MyString"
+latitude_high 1.5
+latitude_low 1.5
+longitude_high 1.5
+longitude_low 1.5
+distance 1.5
+  end
+
+end
