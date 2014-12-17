ad316d6075eb8d48d931dc59394063e0ac6d9668
Marcus Rugger <marcusrugger@yahoo.com>
2014-12-16 23:19:11 -0500

    Add unit tests

--- spec/factories/commutes.rb
+++ spec/factories/commutes.rb
@@ -1,13 +1,15 @@
+require 'factory_girl_rails'
+
 FactoryGirl.define do
-  factory :commute do
+  factory :commute, :class => 'Commute' do
     start_time 1
-end_time 1
-objective "MyString"
-latitude_high 1.5
-latitude_low 1.5
-longitude_high 1.5
-longitude_low 1.5
-distance 1.5
+    end_time 1
+    objective "MyString"
+    latitude_high 1.5
+    latitude_low 1.5
+    longitude_high 1.5
+    longitude_low 1.5
+    distance 1.5
   end
 
 end
--- spec/models/commute_spec.rb
+++ spec/models/commute_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
+
 RSpec.describe Commute, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'factory is valid' do
+    subject(:trip) { FactoryGirl.create(:commute) }
+
+    it { is_expected.to be_valid }
+    it { expect(subject.start_time).to equal(1) }
+
+    context "trip" do
+      subject { trip.start_time }
+      it { is_expected.to equal(1) }
+    end
+  end
+
+  describe 'model' do
+    before { FactoryGirl.create(:commute) }
+    it { expect(Commute.count).to equal(1) }
+  end
 end
